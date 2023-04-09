5287f74728fe88761deb31f900c92503d94fb3d1
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-04-08 22:58:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    return Math.abs(location-42);
+}
+
+function distanceFromHqInFeet(location){
+    return 264 * distanceFromHqInBlocks(location);
+}
+
+function distanceTravelledInFeet(start,destination){
+    return 264 * Math.abs(destination-start);
+}
+
+function calculatesFarePrice(start,destination){
+    if(distanceTravelledInFeet(start,destination)<=400 ){
+        return 0;
+    }
+    else if(distanceTravelledInFeet(start,destination)>400 && distanceTravelledInFeet(start,destination)<=2000){
+        return (distanceTravelledInFeet(start,destination)-400)* .02;
+    }
+    else if(distanceTravelledInFeet(start,destination)>2000 && distanceTravelledInFeet(start,destination)<=2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
